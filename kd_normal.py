dcda4bf9dda4453f63892cf5187f48f5676c2ad0
Hongmeng Yam <yam@DN0a1fe313.SUNet>
2024-09-17 02:23:05 -0700

    kd_normal

--- kd_normal.py
+++ kd_normal.py
@@ -32,11 +32,11 @@ ALPHA = 0.5
 
 PATH = Path("./")
 
-teacher_dir1 = PATH / './models/Llama-360M'
+teacher_dir1 = PATH / './models/Llama-360M-G10'
 teacher_dir2 = PATH / './models/GPT2-705M'
 
 
-MODEL_NAME = f'Baby-Llama-58M'
+MODEL_NAME = f'Baby-Llama-58M-G10'
 MODEL_OUTPUT = Path('./models') /  MODEL_NAME
 EVAL_SAMPLES = 8192
 
