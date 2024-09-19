dbf645bc2020fb2fe3601d8f1e03ee5a2ff16980
Hongmeng Yam <yam@DN0a1fe313.SUNet>
2024-09-19 01:08:24 -0700

    llama-3b

--- kd_3teachers.py
+++ kd_3teachers.py
@@ -33,7 +33,7 @@ teacher_dir1 = PATH / './models/Llama-360M-G10'
 teacher_dir2 = PATH / './models/GPT2-705M'
 teacher_dir3 = PATH / './models/Electra-705M'  # Path to the ELECTRA model
 
-MODEL_NAME = f'Baby-Llama-58M-G10'
+MODEL_NAME = f'Baby-Llama-3-teachers'
 MODEL_OUTPUT = Path('./models') / MODEL_NAME
 EVAL_SAMPLES = 8192
 
