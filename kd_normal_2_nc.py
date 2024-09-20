ba92521b8ecbafa88acc42e4212ebdcebc2ba7f6
Hongmeng Yam <yam@DN0a1fe313.SUNet>
2024-09-19 20:31:41 -0700

    self-critique

--- kd_normal_2_nc.py
+++ kd_normal_2_nc.py
@@ -10,7 +10,7 @@ from transformers import (
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Subset
+from torch.utils.data import  Subset
 from random import sample
 
 from pathlib import Path
@@ -21,53 +21,86 @@ from babylm_dataset import BabylmDataset
 
 
 #############
-LR = 5e-5  # Reduced for fine-tuning
-BATCH_SIZE = 16  # Reduced batch size for faster convergence
+LR = 2.5e-4
+BATCH_SIZE = 32
 SEQ_LENGTH = 128
 
-TEMPERATURE = 1.5  # Lower temperature for sharper logits during fine-tuning
-ALPHA = 0.5  # Same alpha for balance between student and teacher loss
+TEMPERATURE = 2.0
+ALPHA = 0.5
 #############
 
-PATH = Path("./")
-teacher_student_dir = PATH / './models/babyllama-58M-real'  # Same directory for both student and teacher
 
-MODEL_NAME = f'Baby-Llama-58M-NC-selfdistill'
+PATH = Path("./")
+
+teacher_dir1 = PATH / './models/TrainAll-2-Nsample-Contrastive_Student/checkpoint-24489'
+teacher_dir2 = PATH / './models/babyllama-58M-real'
+
+
+MODEL_NAME = f'second-baby-similar-parent'
 MODEL_OUTPUT = Path('./models') /  MODEL_NAME
-EVAL_SAMPLES = 1024  # Reduced sample size for faster evaluation
+EVAL_SAMPLES = 8192
+
 
 wandb_log = True
 
-# Tokenizer setup
+
+
 tokenizer_path = PATH / "models/gpt-clean-16000.json"
-tokenizer = GPT2TokenizerFast(tokenizer_file=str(tokenizer_path))
+tokenizer = GPT2TokenizerFast(tokenizer_file= str(tokenizer_path))
 tokenizer.bos_token = "<s>"
 tokenizer.eos_token = "</s>"
 tokenizer.pad_token = "<pad>"
 
-# Dataset setup
+# in the original code I had random_chunk = False
+# random_chunk=True is expected to improve the model performance a bit
 train_dataset = BabylmDataset(PATH / "data/babylm_10M_clean", SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
 full_eval_dataset = BabylmDataset(PATH / "data/babylm_dev_clean", SEQ_LENGTH, tokenizer=tokenizer, offset=0)
 
 eval_indices = sample(range(len(full_eval_dataset)), EVAL_SAMPLES)
 eval_dataset = Subset(full_eval_dataset, eval_indices)
 
+
+
+
 tokenizer.model_max_length = SEQ_LENGTH
 
-# Load the model for both student and teacher
-student = LlamaForCausalLM.from_pretrained(teacher_student_dir)
-teacher = LlamaForCausalLM.from_pretrained(teacher_student_dir)
-teachers = [teacher]
+config = LlamaConfig(
+    vocab_size=tokenizer.vocab_size,
+    hidden_size=512,
+    num_hidden_layers=16,
+    intermediate_size=1024,
+    num_attention_heads=8,
+    bos_token_id=tokenizer.convert_tokens_to_ids("<s>"),
+    eos_token_id=tokenizer.convert_tokens_to_ids("</s>"),
+    pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
+    max_position_embeddings=2*SEQ_LENGTH,
+)
+
+student = LlamaForCausalLM(config)
+# student = LlamaForCausalLM.from_pretrained(student_dir)
+
+
+teacher1 = LlamaForCausalLM.from_pretrained(teacher_dir1)
+teacher2 = LlamaForCausalLM.from_pretrained(teacher_dir2)
+teachers = [teacher1, teacher2]
+
 
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer, mlm=False,
 )
 
+
 print(f'model num parameters: student = {student.num_parameters()}')
-print(f'model num parameters: teacher = {teacher.num_parameters()}')
+print(f'model num parameters: teacher1 = {teacher1.num_parameters()}')
+print(f'model num parameters: teacher2 = {teacher2.num_parameters()}')
 
 
-# Distillation Trainer setup (same as before but using self-distillation)
+
+#  Distillation Trainer
+#  We modified the Trainer from this repo https://github.com/philschmid/knowledge-distillation-transformers-pytorch-sagemaker
+# to work with an ensemble of teachers
+
+
 class DistillationTrainingArguments(TrainingArguments):
     def __init__(self, *args, alpha=0.5, temperature=2.0, **kwargs):
         super().__init__(*args, **kwargs)
@@ -80,21 +113,25 @@ class DistillationTrainer(Trainer):
         super().__init__(*args, **kwargs)
         self.teachers = teacher_models
         for teacher in self.teachers:
+            # place each teacher on same device as student
             self._move_model_to_device(teacher, self.model.device)
             teacher.eval()
 
     def compute_loss(self, model, inputs, return_outputs=False):
-        # Compute student output
+        # compute student output
         outputs_student = model(**inputs)
         student_loss = outputs_student.loss
 
-        # Compute teacher output
+        # compute teacher output
         with torch.no_grad():
             all_teacher_logits = []
             for teacher in self.teachers:
                 outputs_teacher = teacher(**inputs)
                 all_teacher_logits.append(outputs_teacher.logits)
             avg_teacher_logits = torch.stack(all_teacher_logits).mean(dim=0)
+
+        # assert size
+        assert outputs_student.logits.size() == avg_teacher_logits.size()
 
         # Soften probabilities and compute distillation loss
         loss_function = nn.KLDivLoss(reduction="batchmean")
@@ -105,7 +142,6 @@ class DistillationTrainer(Trainer):
             )
             * (self.args.temperature ** 2)
         )
-
         # Return weighted student loss
         loss = self.args.alpha * student_loss + (1.0 - self.args.alpha) * loss_logits
         return (loss, outputs_student) if return_outputs else loss
@@ -116,38 +152,55 @@ if wandb_log:
     wandb.init(project='babylm', name=MODEL_NAME)
 
 
+
+
+
 training_args = DistillationTrainingArguments(
     output_dir=MODEL_OUTPUT,
     overwrite_output_dir=True,
-    save_strategy="epoch",
-    evaluation_strategy=None,
-    num_train_epochs=3,  # Reduced for faster training
-    gradient_accumulation_steps=2,  # Higher for larger effective batch size
+    save_strategy = "epoch",
+    evaluation_strategy = "epoch",
+    num_train_epochs=6,
+    gradient_accumulation_steps=1,
     per_device_train_batch_size=BATCH_SIZE,
-    save_total_limit=1,  # Set to zero to avoid saving too many checkpoints
+    save_total_limit=1,  # Set to zero to avoid saving
     report_to="wandb",
-    warmup_steps=100,  # Shortened warmup
+    warmup_steps=200, 
     lr_scheduler_type="cosine",
     learning_rate=LR,
     logging_steps=20,
-    fp16=True,  # Enable mixed precision for faster training
+    fp16=True,
     load_best_model_at_end=False,
     metric_for_best_model="eval_loss",
-    weight_decay=0.05,  # Adjusted for fine-tuning
+    weight_decay=0.1,
     alpha=ALPHA,
     temperature=TEMPERATURE,
 )
 
+
 trainer = DistillationTrainer(
-    student,
-    training_args,
-    teacher_models=teachers,  # Using the same model as teacher and student
-    data_collator=data_collator,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-)
+        student,
+        training_args,
+        teacher_models=teachers,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+
+    )
+
 
 trainer.train()
 
+
 trainer.save_model(MODEL_OUTPUT)
 tokenizer.save_pretrained(MODEL_OUTPUT)
+
+
+
+
+
+
+
+
+
+
